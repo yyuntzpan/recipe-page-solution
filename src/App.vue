e7ce28a15664ba9c632aad0d26cd7e046035c81e
yyuntzpan <yap0602@gmail.com>
2024-12-09 15:38:35 +0800

    [edit] 更新檔案路徑名稱

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import Recipe from "./components/recipe.vue";
+import Recipe from "./components/Recipe.vue";
 </script>
 
 <template>
-  
-    <Recipe />
-  
+  <Recipe />
 </template>
 
 <style scoped></style>
